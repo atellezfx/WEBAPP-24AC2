bf9ec72fd915ba95bf9298089cb82cff1b92ba41
Alejandro Téllez <alejandro.tellez@gmail.com>
2024-07-25 21:30:52 -0600

    Código OpenClass S4

--- frontend/src/app/components/catalogo/catalogo.component.ts
+++ frontend/src/app/components/catalogo/catalogo.component.ts
@@ -1,27 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { Mascota } from '../../models/mascota';
-import { mascotasPrueba } from '../../util/datos-prueba';
 import { MascotaComponent } from "../mascota/mascota.component";
+import { MascotaService } from '../../services/mascota.service';
+import { PropietarioService } from '../../services/propietario.service';
+import { Propietario } from '../../models/propietario';
+import { OperatorFunction, Observable, debounceTime, distinctUntilChanged, filter, map } from 'rxjs';
+import { NgbTypeaheadModule, NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+export const PROP_KEY = 'propietarioActual';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [MascotaComponent],
+  imports: [MascotaComponent, NgbTypeaheadModule, FormsModule],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
 export class CatalogoComponent implements OnInit {
 
-  mascotas:Mascota[];
+  mascotas:Mascota[] = [];
+  propietarios:Propietario[] = [];
+  modelo!: Propietario;
 
-  constructor() {
-    // TODO: Reemplazarlo por una llamada al método lista() del servicio de mascotas
-    this.mascotas = mascotasPrueba;
-  }
+  formatter = (prop:Propietario) => `${prop.nombre} ${prop.apellidos} <${prop.email}>`;
+
+  constructor( private mascotaSvc:MascotaService, private propSvc:PropietarioService ) { }
 
   public ngOnInit(): void{
-    // TODO: Implementar la lógica para cargar todos los propietarios
+    const jsonDoc = sessionStorage.getItem(PROP_KEY);
+    this.propSvc.lista().subscribe( props => this.propietarios = props );
+    if( jsonDoc ) {
+      this.modelo = JSON.parse( jsonDoc );
+      this.refrescarTarjetas( this.modelo );
+    }
   }
 
+  public actualizaMascotas(event:NgbTypeaheadSelectItemEvent):void {
+    sessionStorage.setItem(PROP_KEY, JSON.stringify( event.item ));
+    this.refrescarTarjetas( event.item );
+  }
+
+  private refrescarTarjetas( prop:Propietario ):void {
+    this.mascotaSvc.lista( prop.id ).subscribe(
+      arreglo => {
+        this.mascotas.length = 0;
+        this.mascotas.push(...arreglo);
+      }
+    );
+  }
+
+  search: OperatorFunction<string, readonly Propietario[]> = (text$: Observable<string>) =>
+		text$.pipe(
+			debounceTime(200),
+			distinctUntilChanged(),
+			filter((term) => term.length >= 2),
+			map((term) => this.propietarios.filter((prop) => new RegExp(term, 'mi').test(prop.email)).slice(0, 10)),
+		);
+
+  public eliminar( mascota:Mascota|null ):void {
+    // TODO: Implementar lógica de quitar la tarjeta de la mascota eliminada
+  }
 
 }
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  // TODO: Implementar componente de LOGIN
+  formulario:FormGroup;
+  mensajeError:string = '';
+
+  constructor( private builder:FormBuilder, private authSvc:AuthService, private router:Router ) {
+    this.formulario = builder.group( {
+      email:['', [ Validators.required, Validators.email ]],
+      password:['', Validators.required ]
+    } );
+  }
+
+  public enviarDatos() {
+    if( this.formulario.valid ) {
+      const credenciales = this.formulario.value;
+      this.authSvc.login( credenciales ).subscribe({
+        next: datos => this.procesarRespuesta( datos, credenciales ),
+        error: datos => this.mensajeError = datos
+      });
+    } else {
+      this.mensajeError = 'Formulario incompleto';
+    }
+  }
+
+  private procesarRespuesta(datos:any, usr:Usuario): void {
+    // TODO: Implementar guardar datos de usuario y token en localStorage
+    this.router.navigateByUrl('/catalogo');
+  }
+
 
 }
